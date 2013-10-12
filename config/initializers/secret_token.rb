b047b2812dc8fffc93d84391d2374df4f0b5b6e8
Erick Brower <erick@berecruited.com>
2013-10-11 21:21:48 -0700

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clockwork::Application.config.secret_key_base = '517c19fac11f6b72ec3cd3b996005a94bc83cb21f0192d6ae497dd212a6504a44589991c82db31bb4f50117fb783984362120d665c618ba16d389b49560cd67e'
