c3a71b3e6106a3b2ed65d333486974c6e2bce7f2
Erick Brower <erick@berecruited.com>
2013-10-20 21:04:05 -0700

    Cleaned up blog_posts views

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -1,29 +1,47 @@
 module BlogPostsHelper
-  def edit_blog_post_button(post)
+  def edit_blog_post_button(blog_post)
     link_to "<span class='glyphicon glyphicon-pencil'></span>".html_safe, 
-            edit_blog_post_path(post), 
-            class: 'btn btn-sm btn-primary'
+      edit_blog_post_path(blog_post), 
+      class: 'btn btn-sm btn-primary'
   end
 
-  def delete_blog_post_button(post)
+  def delete_blog_post_button(blog_post)
     link_to "<span class='glyphicon glyphicon-remove'></span>".html_safe, 
-            blog_post_path(post), 
-            method: :delete, 
-            class: 'btn btn-sm btn-danger'
-
+      blog_post_path(blog_post), 
+      method: :delete, 
+      class: 'btn btn-sm btn-danger'
   end
 
-  def status_label(post)
-    case post.status.to_sym
-    when :published then "<span class='label label-success'>#{post.status.to_s.humanize}</span>".html_safe
-    when :draft then "<span class='label label-warning'>#{post.status.to_s.humanize}</span>".html_safe
-    when :raw then "<span class='label label-default'>#{post.status.to_s.humanize}</span>".html_safe
+  def publish_combo_button_for(blog_post, locals)
+    all = { blog_post: blog_post }
+    all.merge(locals)
+    render partial: 'blog_posts/publish_combo_button', locals: all
+  end
+
+  def blog_post_header_title_for(blog_post)
+    link_to blog_post.title, blog_post_path(blog_post)
+  end
+
+  def blog_post_sub_header_for(blog_post)
+    author_link = link_to "#{blog_post.author.profile.first_name} #{blog_post.author.profile.last_name}", person_path(blog_post.author)
+    "By #{author_link} at #{blog_post.created_at}".html_safe
+  end
+
+  def status_label(blog_post)
+    case blog_post.status.to_sym
+    when :published then "<span class='label label-success'>#{blog_post.status.to_s.humanize}</span>".html_safe
+    when :draft then "<span class='label label-warning'>#{blog_post.status.to_s.humanize}</span>".html_safe
+    when :raw then "<span class='label label-default'>#{blog_post.status.to_s.humanize}</span>".html_safe
     end
   end
 
-  def render_blog_post_tags(post)
+  def blog_post_comments(blog_post)
+    render partial: 'blog_posts/comments', locals: { blog_post: blog_post }
+  end
+
+  def blog_post_tags_for(blog_post)
     tags = ''
-    post.tags.each do |tag|
+    blog_post.tags.each do |tag|
       tags += "<span class='label label-info'>#{tag}</span> "
     end
     tags.html_safe
